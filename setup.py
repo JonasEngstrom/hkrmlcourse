bb2fd530794515e10809f9d9cddb0ed5d49d27c3
Jonas Engström <56550304+JonasEngstrom@users.noreply.github.com>
2021-10-27 16:05:42 +0200

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,13 @@
+# setup.py
+import setuptools
+
+setuptools.setup(name="hkrmlcourse",
+      version="0.1",
+      description="Helper functions for ML course.",
+      url="https://github.com/JonasEngstrom/hkrmlcourse",
+      author="Jonas Engström",
+      author_email="JonasEngstrom@users.noreply.github.com",
+      license="MIT",
+      package_dir={"": "src"},
+      packages=setuptools.find_packages(where="src")
+      )
