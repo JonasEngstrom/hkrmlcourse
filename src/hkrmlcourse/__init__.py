523480ecbbc9151e7280e4bc52b224295b267405
Jonas Engström <56550304+JonasEngstrom@users.noreply.github.com>
2021-10-29 15:56:11 +0200

    Added generate_appreciation_column.py

--- src/hkrmlcourse/__init__.py
+++ src/hkrmlcourse/__init__.py
@@ -1,6 +1,7 @@
 __all__ = ['days_ago',
            'drop_old_stocks',
            'drop_young_stocks',
+           'generate_appreciation_column',
            'generate_appreciation_labels',
            'generate_multiindex',
            'get_sp500_history',
--- src/hkrmlcourse/generate_appreciation_column.py
+++ src/hkrmlcourse/generate_appreciation_column.py
@@ -1,29 +1,28 @@
-# generate_appreciation_labels.py
+# generate_appreciation_column.py
 
 import pandas
 
 import hkrmlcourse.generate_multiindex
 
-def generate_appreciation_labels(stock_history: pandas.DataFrame,
+def generate_appreciation_column(stock_history: pandas.DataFrame,
                                  shift_days: int = -1) -> pandas.DataFrame:
     """
     Takes a stock history data frame from Yahoo Finance and returns a data
-    frame with boolean labels whether the stock appreciated the day after
-    (with default settings).
+    frame with the stock appreciation the day after (with default settings).
 
     :param stock_history: Stock history from Yahoo Finance.
     :type stock_history: pandas.DataFrame
     :param shift_days: Number of days to shift labels (default -1).
     :type shift_days: int
-    :return: A labels indexed by date and ticker symbols.
+    :return: Appreciation indexed by date and ticker symbols.
     :rtype: pandas.DataFrame
     """
-    label_data_frame = stock_history.Close / stock_history.Open - 1 > 0
+    label_data_frame = stock_history.Close / stock_history.Open - 1
     shifted_data_frame = label_data_frame.shift(shift_days)
 
     column_titles = hkrmlcourse.generate_multiindex.generate_multiindex(
         shifted_data_frame,
-        f"BoolIntraDayAppreciation{abs(shift_days)}Day"
+        f"PctIntraDayAppreciation{abs(shift_days)}Day"
         f"{'s' if abs(shift_days) != 1 else ''}InThe"
         f"{'Future' if shift_days <= 0 else 'Past'}"
     )
