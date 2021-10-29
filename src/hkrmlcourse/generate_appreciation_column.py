3c87eafeb2c2e37688bdd7a7bf5f8ddf06276e7c
Jonas Engström <56550304+JonasEngstrom@users.noreply.github.com>
2021-10-29 16:45:38 +0200

    Rewrote generate_appreciation_column completely (NB, it works differently now, old code won't work).

--- src/hkrmlcourse/generate_appreciation_column.py
+++ src/hkrmlcourse/generate_appreciation_column.py
@@ -4,29 +4,22 @@ import pandas
 
 import hkrmlcourse.generate_multiindex
 
-def generate_appreciation_column(stock_history: pandas.DataFrame,
-                                 shift_days: int = -1) -> pandas.DataFrame:
+def generate_appreciation_column(stocks: pandas.DataFrame) -> pandas.DataFrame:
     """
-    Takes a stock history data frame from Yahoo Finance and returns a data
-    frame with the stock appreciation the day after (with default settings).
+    Creates a column of intra day appreciation for data from Yahoo Finance.
 
-    :param stock_history: Stock history from Yahoo Finance.
-    :type stock_history: pandas.DataFrame
-    :param shift_days: Number of days to shift labels (default -1).
-    :type shift_days: int
+    :param stocks: Stock history from Yahoo Finance.
+    :type stocks: pandas.DataFrame
     :return: Appreciation indexed by date and ticker symbols.
     :rtype: pandas.DataFrame
     """
-    label_data_frame = stock_history.Close / stock_history.Open - 1
-    shifted_data_frame = label_data_frame.shift(shift_days)
+    appreciation = stocks.Close / stocks.Open - 1
 
     column_titles = hkrmlcourse.generate_multiindex.generate_multiindex(
-        shifted_data_frame,
-        f"PctIntraDayAppreciation{abs(shift_days)}Day"
-        f"{'s' if abs(shift_days) != 1 else ''}InThe"
-        f"{'Future' if shift_days <= 0 else 'Past'}"
+        stocks,
+        'IntraDayAppreciation'
     )
 
-    shifted_data_frame.columns = column_titles
+    appreciation.columns = column_titles
 
-    return shifted_data_frame
+    return appreciation
