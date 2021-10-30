10f6112fb34427cc1923887d0bd0768868dd3d0d
Jonas Engström <56550304+JonasEngstrom@users.noreply.github.com>
2021-10-30 09:52:10 +0200

    Added setup_tpu.py

---
+++ src/hkrmlcourse/setup_tpu.py
@@ -0,0 +1,20 @@
+# setup_tpu.py
+
+import os
+import tensorflow as tf
+from tensorflow import keras
+
+def setup_tpu() -> tf.distribute.TPUStrategy:
+    """
+    Sets up TPU for usage in Google Colab. Returns a TPU strategy.
+
+    :return: A TPU strategy.
+    :rtype: tf.distribute.TPUStrategy
+    """
+    TF_MASTER = f"grpc://{os.environ['COLAB_TPU_ADDR']}"
+
+    resolver = tf.distribute.cluster_resolver.TPUClusterResolver(TF_MASTER)
+    tf.config.experimental_connect_to_cluster(resolver)
+    tf.tpu.experimental.intitialize_tpu_system(resolver)
+
+    return tf.distribute.TPUStrategy(resolver)
